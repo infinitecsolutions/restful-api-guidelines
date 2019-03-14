8c3608cfbc345f5c004e5f779deff1aaefaf5e7b
Sören Blom <soeren.blom@infinitec.solutions>
2019-03-15 00:21:08 +0100

    Removes build and link to other output formats
    The Zalando documentation always generated PDFs and EPUB along with the
    HTML output and linked it. For now we don't need it and so it is removed
    to save build time.

--- build.sh
+++ build.sh
@@ -15,14 +15,9 @@ docker pull asciidoctor/docker-asciidoctor
 ./check_rule_ids.sh
 
 docker run -v ${SCRIPT_DIR}:/documents/ asciidoctor/docker-asciidoctor asciidoctor -D /documents/output index.adoc
-docker run -v ${SCRIPT_DIR}:/documents/ asciidoctor/docker-asciidoctor asciidoctor-pdf -D /documents/output index.adoc
-docker run -v ${SCRIPT_DIR}:/documents/ asciidoctor/docker-asciidoctor asciidoctor-epub3 -D /documents/output index.adoc
 
 cp ./LICENSE ${BUILD_DIR}/
 cp -r assets ${BUILD_DIR}/
 cp -r -n legacy/* ${BUILD_DIR}/
 
 ./generate_rules_json.sh
-
-mv ${BUILD_DIR}/index.pdf ${BUILD_DIR}/zalando-guidelines.pdf
-mv ${BUILD_DIR}/index.epub ${BUILD_DIR}/zalando-guidelines.epub
